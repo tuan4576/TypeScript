87dddc4e6549d53fcc0127ee692c310a09f72baf
thanhtuandev <pro03tuan@gmail.com>
2025-04-08 22:37:13 +0700

    type

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,26 @@
 interface face{
     x: number;
     y: number;
-    z: string
-}
+    z: unknown;
+    k: unknown;
+}   
 
 const test : face = {
     x : 5,
     y : 6,
-    z : "hello"
+    z : 3,
+    k : "hello nè"
+
 }
 
 function test1(avarible : face){
-    
-    console.log(avarible.x)
+    if(typeof(avarible.z)==="number"){
+        // console.log("là số : " + avarible.z)
+        // console.log("kết quả là :" +)
+    }
+    // console.log(typeof(avarible.y))
+    let chieudai = (avarible.k as string)
+    console.log("chuyển đổi thành  : " + typeof(chieudai))
 }
 
 test1(test)
