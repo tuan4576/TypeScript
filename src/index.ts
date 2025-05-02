09e5c2ed92904783669be6a59c40f204ebe00a40
thanhtuandev <pro03tuan@gmail.com>
2025-05-02 08:31:45 +0700

    array

--- src/index.ts
+++ src/index.ts
@@ -14,13 +14,17 @@ const test : face = {
 }
 
 function test1(avarible : face){
+    let test : (string | number | boolean)[] = ["hello", 1, true];
+    let num : readonly Number[] = [1,2,3,4]
+    let num1 : Number = num[0]
+    test.push("hello nè")
+    // console.log(test);
+    console.log(num);
+    // console.log(num1);
     if(typeof(avarible.z)==="number"){
-        // console.log("là số : " + avarible.z)
-        // console.log("kết quả là :" +)
     }
-    // console.log(typeof(avarible.y))
     let chieudai = (avarible.k as string)
-    console.log("chuyển đổi thành  : " + typeof(chieudai))
+    // console.log("chuyển đổi thành  : " + typeof(chieudai))
 }
 
 test1(test)
